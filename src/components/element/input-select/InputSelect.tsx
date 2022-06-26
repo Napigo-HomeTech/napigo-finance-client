f9af4a554b8398a1af24e7bcf5f2b4e23b0d5da9
Napihup <programminghup@gmail.com>
2022-06-27 02:13:35 +0800

    Prepare code for Todo

--- src/components/element/input-select/InputSelect.tsx
+++ src/components/element/input-select/InputSelect.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { FaCaretDown } from "react-icons/fa";
 import { Button } from "../button/Button";
 import { ClickAwayListener } from "../../common";
@@ -15,15 +15,39 @@ interface InputSelectProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   editable?: boolean;
 }
+/**
+ * TODO: To add key board navigation on the dropdown
+ * @param props
+ * @returns
+ */
 export const InputSelect: React.FC<InputSelectProps> = (props) => {
   const { options, defaultSelected, onSelectChange, label, editable = true, ...inputProps } = props;
 
   const [open, setOpen] = useState<boolean>(false);
   const [selected, setSelected] = useState<Option>(defaultSelected);
 
+  const handleKeydown = (ev: any) => {
+    const key = ev.key;
+
+    if (key === "ArrowDown") {
+      // TODO
+    }
+    if (key === "ArrowUp") {
+      // TODO
+    }
+  };
+
   useCallback(() => {
     onSelectChange?.(selected);
   }, [onSelectChange, selected]);
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeydown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeydown);
+    };
+  }, [open]);
 
   return (
     <div className={`w-full ${editable ? "" : "pointer-events-none"}`}>
@@ -63,16 +87,19 @@ export const InputSelect: React.FC<InputSelectProps> = (props) => {
           p-2`}
           >
             <ul className="list-none m-0 p-0" role="menu">
-              {options.map((item: Option) => {
+              {options.map((item: Option, index) => {
                 return (
                   <li
+                    data-name="input-select-item"
+                    tabIndex={0}
+                    id={item.value}
                     key={item.value}
                     role="menuitem"
                     onClick={() => {
                       setSelected(item);
                       setOpen(false);
                     }}
-                    className={`btn btn-block btn-ghost active:bg-primary`}
+                    className={`btn btn-block btn-ghost active:bg-primary focus:bg-base-content/20 focus:outline-none`}
                   >
                     {item.text}
                   </li>
