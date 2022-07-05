ca6a7571ab863af83bdbf5f041d2b3d17997c422
Napihup <programminghup@gmail.com>
2022-07-05 09:11:05 +0800

    Fix the layout on temporary Drawer Toggle - will relocate this toggle on the user setting screen instead in the future

--- src/components/module/drawer/Drawer.tsx
+++ src/components/module/drawer/Drawer.tsx
@@ -25,22 +25,26 @@ export const Drawer: React.FC<DrawerProps> = (props) => {
         animate={{ width: collapse ? DRAWER_COLLAPSE_WIDTH : DRAWER_EXPAND_WIDTH }}
         transition={{ delay: collapse ? 0.15 : 0 }}
         className={`border-r-[0.5px]
-      border-base-content/20 
-      w-[260px]
-      bg-base-200 fixed
-      top-0 
-      h-screen
-      flex 
-      flex-col 
-      justify-between
-      transition
+        border-base-content/20 
+        w-[260px]
+        bg-base-200 fixed
+        top-0 
+        
+        h-screen
+        flex 
+        flex-col 
+        justify-between
+        transition
       `}
       >
         <div className="w-full">
           <LogoSection collapse={collapse} />
           <DrawerMenu collapse={collapse} />
         </div>
-        <Toggle defaultChecked onChange={onToggle} />
+
+        <div className="flex w-full flex-col h-auto p-4 items-start justify-center">
+          <Toggle defaultChecked onChange={onToggle} />
+        </div>
       </motion.div>
     </AnimatePresence>
   );
