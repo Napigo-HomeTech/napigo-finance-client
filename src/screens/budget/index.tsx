dc118234adb71275c4490ed1259bf8925ff27447
Napihup <programminghup@gmail.com>
2022-07-06 23:38:40 +0800

    added sample menupopper usage

--- src/screens/budget/index.tsx
+++ src/screens/budget/index.tsx
@@ -1,9 +1,23 @@
+import { Button, ButtonPopper, MenuPopper } from "components/element";
 import React from "react";
+import { FaUser } from "react-icons/fa";
 
 const BudgetScreen: React.FC = () => {
   return (
     <div className="flex flex-row items-center justify-center h-[500px] w-full">
       <p className="font-bold text-2xl">Hello Budget</p>
+
+      <ButtonPopper renderToggler={() => <Button text="test" />}>
+        <div>Hello There</div>
+      </ButtonPopper>
+      <MenuPopper
+        renderToggler={() => <Button text="test" />}
+        items={[
+          { text: "Item One", icon: <FaUser />, onClick: () => {} },
+          { text: "Item One", icon: <FaUser />, onClick: () => {} },
+          { text: "Item One", icon: <FaUser />, onClick: () => {} },
+        ]}
+      />
     </div>
   );
 };
